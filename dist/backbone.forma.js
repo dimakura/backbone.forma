876452f6a6641779638f017e13a25762d1722d54
Dimitri Kurashvili <dimakura@gmail.com>
2014-09-11 11:45:05 +0400

    adding example application: first version

--- dist/backbone.forma.js
+++ dist/backbone.forma.js
@@ -1,7 +1,13 @@
 !function(exports, global) {
     global.Forma = exports;
-    var Field = function(name) {
-        this.name = name;
+    var FormaError = function(message) {
+        this.name = "FormaError", this.message = message, this.stack = new Error().stack;
+    };
+    FormaError.prototype = new Error(), FormaError.prototype.constructor = FormaError, 
+    exports.FormaError = FormaError;
+    var Field = function(options) {
+        if (!options) throw new exports.FormaError("No properties specified for the field");
+        this.name = options.name, this.type = options.type;
     };
     Field.prototype.template = function() {
         return _.template('<input name="' + this.name + '">');
--- src/field.js
+++ src/field.js
@@ -1,9 +1,16 @@
-var Field = function(name) {
-  this.name = name;
+var FIELD_TYPES = [
+  'text',
+  'password',
+];
+
+var Field = function(options) {
+  if(!options) { throw new exports.FormaError('No properties specified for the field'); }
+  this.name = options.name;
+  this.type = options.type || 'text';
 };
 
 Field.prototype.template = function() {
-  return _.template('<input name="' + this.name + '">');
+  return _.template('<input name="' + this.name + '" type="' + this.type + '"">');
 };
 
 exports.Field = Field;
