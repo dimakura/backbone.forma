31d593b98f49a92f330e43392ad91963f831017a
Dimitri Kurashvili <dimakura@gmail.com>
2014-09-12 07:15:27 +0400

    set forma styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -29,6 +30,16 @@ module.exports = function(grunt) {
           beautify: true,
           mangle: false,
           wrap: 'forma'
+        }
+      }
+    },
+
+    less: {
+      development: {
+        options: {
+          paths: ['less'],
+        }, files: {
+          'backbone.forma.css': 'less/forma.less'
         }
       }
     },
